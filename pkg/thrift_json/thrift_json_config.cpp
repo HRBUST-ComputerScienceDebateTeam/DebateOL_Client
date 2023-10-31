0e2f2cc808a328ba76ff024eec797cacd3600bc6
TTaket <575500603@qq.com>
2023-10-31 17:33:18 +0800

    feat: add video.thrift

---
+++ pkg/thrift_json/thrift_json_config.cpp
@@ -0,0 +1,81 @@
+#include "./thrift_json_config.h"
+#include <QString>
+#include <stdlib.h>
+#include <QDebug>
+
+//通过生成代码产生冗余来解决反射问题
+template<typename VAL_TYPE>
+inline std::string gen_jsontoken(int id, std::string type_name , VAL_TYPE val){
+    string ret;
+    ret += "\"" + QString().setNum(id).toStdString() + "\":";
+    ret += "{\"" + type_name +"\":";
+    ret +=  QString().setNum(val).toStdString() ;
+    ret += "}";
+    return ret;
+}
+
+inline std::string gen_jsontoken(int id, std::string type_name , char val){
+    string ret;
+    ret += "\"" + QString().setNum(id).toStdString() + "\":";
+    ret += "{\"" + type_name +"\":";
+    ret += "\'" + QString(val).toStdString() +"\'";
+    ret += "}";
+    return ret;
+}
+
+inline std::string gen_jsontoken(int id, std::string type_name , string val){
+    string ret;
+    ret += "\"" + QString().setNum(id).toStdString() + "\":";
+    ret += "{\"" + type_name +"\":";
+    ret += "\"" + val +"\"";
+    ret += "}";
+    return ret;
+}
+
+template<typename VAL_TYPE>
+void get_tokenval(const string &s , int l , int r , VAL_TYPE & typeval ){
+    //"2":{"str":"hi"}
+    int aimpos = 0;
+    for(int i = l;i<=r;i++){
+        if(s[i] == ':' && s[i+1] != '{'){
+            aimpos = i;
+            break;
+        }
+    }
+    string_view ss = s;
+
+    string base =string(ss.substr(aimpos+1 , r - aimpos -1 ));
+    //qDebug().noquote() << aimpos+1 <<" " <<  r <<" " << base;
+    typeval = VAL_TYPE(atoi(string(base).c_str()));
+}
+
+void get_tokenval(const string &s , int l , int r , char & typeval ){
+    //"2":{"str":"hi"}
+    int aimpos = 0;
+    for(int i = l;i<=r;i++){
+        if(s[i] == ':' && s[i+1] != '{'){
+            aimpos = i;
+            break;
+        }
+    }
+    char ch = s[aimpos+2];
+    typeval= char(ch);
+}
+
+void get_tokenval(const string &s , int l , int r , string & typeval ){
+    //"2":{"str":"hi"}
+    int aimpos = 0;
+    for(int i = l;i<=r;i++){
+        if(s[i] == ':' && s[i+1] != '{'){
+            aimpos = i;
+            break;
+        }
+    }
+    string_view ss = s;
+
+    string base =string(ss.substr(aimpos+2 , r - aimpos -3 ));
+    qDebug().noquote() << aimpos+1 <<" " <<  r <<" " << base;
+    typeval= base;
+}
+
+
--- pkg/thrift_json/thrift_json_config.h
+++ pkg/thrift_json/thrift_json_config.h
@@ -11,8 +11,25 @@ public:
     std::map<int , std::string> mp3;//id - 类型
 };
 
+//通过生成代码产生冗余来解决反射问题
+template<typename VAL_TYPE>
+inline std::string gen_jsontoken(int id, std::string type_name , VAL_TYPE val);
 
-// 每个结构体
+inline std::string gen_jsontoken(int id, std::string type_name , char val);
+
+inline std::string gen_jsontoken(int id, std::string type_name , string val);
+
+template<typename VAL_TYPE>
+void get_tokenval(const string &s , int l , int r , VAL_TYPE & typeval );
+
+void get_tokenval(const string &s , int l , int r , char & typeval );
+
+void get_tokenval(const string &s , int l , int r , string & typeval );
+
+
+
+
+// 每个结构体 - Echo
 class Echo_SendInfo{
 public:
     static JSON_Base json_base_;
@@ -23,6 +40,7 @@ public:
     uint32_t id;
     string info;
 };
+
 
 class Echo_RecvInfo{
 public:
@@ -35,4 +53,5 @@ public:
     string info;
     string time;
 };
+
 
